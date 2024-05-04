c7292b80c7babd434200006dee31ac1c9ddbaef0
clementb49 <clement.boussiron@gmail.com>
2024-05-05 00:56:01 +0200

    feat(bkild_executable): Getting the release number from the SCM

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from cx_Freeze import setup
-from src.consts import APP_VERSION
 
 
 setup(
-	version=APP_VERSION,
 	message_extractors={
 		"src": [("**.py", "python", None), ("**.pyw", "python", None)]
-	},
+	}
 )
