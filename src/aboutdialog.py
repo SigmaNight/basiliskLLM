93a09cd171dc918dd5425d55be003e7a69a2b0f1
clementb49 <clement.boussiron@gmail.com>
2024-05-05 00:58:17 +0200

    refactor(about_dialog): Use the wx about dialog and get the version from SCM

---
+++ src/aboutdialog.py
@@ -0,0 +1,29 @@
+import sys
+import wx
+from consts import APP_NAME, APP_SOURCE_URL
+
+app_version = None
+if getattr(sys, "frozen", False):
+	from BUILD_CONSTANTS import *  # noqa # type: ignore
+
+	app_version = BUILD_RELEASE_STRING  # noqa # type: ignore
+else:
+	from setuptools_scm import get_version
+
+	app_version = get_version(root='..', relative_to=__file__)
+
+APP_AUTHORS = ["André-Abush Clause", "Clément Boussiron" "Nael Sayegh"]
+
+APP_TRANSLATORS = ["André-Abush Clause", "Clément Boussiron"]
+
+
+def display_about_dialog(parent):
+	info = wx.adv.AboutDialogInfo()
+	info.SetName(APP_NAME)
+	info.SetVersion(app_version)
+	info.SetDescription(_("Where LLMs Unite"))
+	info.SetWebSite(APP_SOURCE_URL)
+	info.SetLicense(_("GPLV2"))
+	info.SetDevelopers(APP_AUTHORS)
+	info.SetTranslators(APP_TRANSLATORS)
+	wx.adv.AboutBox(info)
