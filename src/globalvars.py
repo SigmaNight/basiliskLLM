33aa983a63e8b4f3419aa0c637b16a5a41a909b5
André-Abush Clause <dev@andreabc.net>
2024-05-12 23:16:44 +0200

    refactor(globalvars): simplify user data path resolution logic

--- src/globalvars.py
+++ src/globalvars.py
@@ -1,13 +1,12 @@
 import sys
 from pathlib import Path
 
-user_data_path = None
-if getattr(sys, "frozen", False):
-	test_user_data = Path(sys.executable).parent / Path("user_data")
-	user_data_path = test_user_data if test_user_data.exists() else None
-else:
-	test_user_data = Path(__file__).parent / Path("user_data")
-	user_data_path = test_user_data if test_user_data.exists() else None
 
+base_path = Path(
+	sys.executable if getattr(sys, "frozen", False) else __file__
+).parent
+user_data_path = (
+	base_path / "user_data" if (base_path / "user_data").exists() else None
+)
 
 args = None
