20fe8f8e314c548f2695da83b4a1588a67647aa0
clementb49 <clement.boussiron@gmail.com>
2024-05-12 22:32:58 +0200

    fix: adding global variable for user data path

--- src/globalvars.py
+++ src/globalvars.py
@@ -1,1 +1,13 @@
+import sys
+from pathlib import Path
+
+user_data_path = None
+if getattr(sys, "frozen", False):
+	test_user_data = Path(sys.executable).parent / Path("user_data")
+	user_data_path = test_user_data if test_user_data.exists() else None
+else:
+	test_user_data = Path(__file__).parent / Path("user_data")
+	user_data_path = test_user_data if test_user_data.exists() else None
+
+
 args = None
