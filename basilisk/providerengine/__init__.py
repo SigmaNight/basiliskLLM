e2d6a8ebcc5ebdc9409d28b6caf4bc28bc78e19c
André-Abush Clause <dev@andreabc.net>
2024-06-02 20:06:04 +0200

    feat(provider): initial support for Anthropic

--- basilisk/providerengine/__init__.py
+++ basilisk/providerengine/__init__.py
@@ -1,4 +1,5 @@
 from .baseengine import BaseEngine as BaseEngine
+from .anthropicengine import AnthropicAIEngine as AnthropicAIEngine
 from .mistralaiengine import MistralAIEngine as MistralAIEngine
 from .openaiengine import OpenAIEngine as OpenAIEngine
 from .openrouterengine import OpenRouterEngine as OpenRouterEngine
