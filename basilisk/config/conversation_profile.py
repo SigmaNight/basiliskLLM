604f14ea576f274772a778f3cd72f7ecd8cf3b1d
clementb49 <clement.boussiron@gmail.com>
2024-08-05 19:11:21 +0200

    feat(conversation_profile): Adding a pydantic model to store the conversation profile configuration

---
+++ basilisk/config/conversation_profile.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from pydantic import BaseModel, Field, RootModel
+
+
+class ConversationProfile(BaseModel):
+	name: str
+	system_prompt: str = Field(default="")
+
+	@classmethod
+	def get_default(cls) -> ConversationProfile:
+		return cls(name="default", system_prompt="default system prompt")
+
+
+class ConversationProfileManager(RootModel):
+	root: list[ConversationProfile] = Field(
+		default_factory=lambda: [ConversationProfile.get_default()]
+	)
