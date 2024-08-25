0330d8588759353bc1b3a293710eab3fc6f5767a
clementb49 <clement.boussiron@gmail.com>
2024-08-25 19:12:49 +0200

    refactor: extract manage data in a specific method in server thread

--- basilisk/server_thread.py
+++ basilisk/server_thread.py
@@ -12,14 +12,14 @@ log = logging.getLogger(__name__)
 
 
 class ServerThread(threading.Thread):
-	def __init__(self, frame, port):
+	def __init__(self, frame: wx.Frame, port: int) -> None:
 		super().__init__()
 		self.frame = frame
 		self.port = port
 		self.running = threading.Event()
 		self.daemon = True
 
-	def run(self):
+	def run(self) -> None:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		if not sock:
 			log.error("Failed to create socket")
@@ -38,51 +38,7 @@ class ServerThread(threading.Thread):
 				try:
 					data = conn.recv(1024 * 1024 * 20)
 					if data:
-						data = data.decode("utf-8")
-						if data.startswith("grab:"):
-							grab_mode = data.split(':', 1)[1].strip()
-							if grab_mode == "full":
-								wx.CallAfter(
-									self.frame.screen_capture, CaptureMode.FULL
-								)
-							elif grab_mode == "window":
-								wx.CallAfter(
-									self.frame.screen_capture,
-									CaptureMode.WINDOW,
-								)
-							elif re.match(
-								r"\d+, ?\d+, ?\d+, ?\d+(?:;.*+)?", grab_mode
-							):
-								name = ""
-								coords = grab_mode
-								if '\n' in grab_mode:
-									coords, name = grab_mode.split('\n', 1)
-								coords = tuple(map(int, coords.split(",")))
-								wx.CallAfter(
-									self.frame.capture_partial_screen,
-									coords,
-									name,
-								)
-							else:
-								log.error(f"Invalid grab mode: {grab_mode}")
-						elif data.startswith("url:"):
-							name = ""
-							url = data.split(':', 1)[1].strip()
-							if '\n' in url:
-								url, name = url.split('\n', 1)
-							image_files = [
-								ImageFile(
-									location=url,
-									name=name,
-									size=-1,
-									dimensions=(0, 0),
-								)
-							]
-							wx.CallAfter(
-								self.frame.current_tab.add_images, image_files
-							)
-						else:
-							log.error(f"no action for data: {data}")
+						self.manage_rcv_data(data)
 						conn.sendall(b"ACK")
 					else:
 						log.error("No data received")
@@ -91,6 +47,39 @@ class ServerThread(threading.Thread):
 				finally:
 					conn.close()
 		sock.close()
+
+	def manage_rcv_data(self, data: bytes) -> None:
+		data = data.decode("utf-8")
+		if data.startswith("grab:"):
+			grab_mode = data.split(':', 1)[1].strip()
+			if grab_mode == "full":
+				wx.CallAfter(self.frame.screen_capture, CaptureMode.FULL)
+			elif grab_mode == "window":
+				wx.CallAfter(self.frame.screen_capture, CaptureMode.WINDOW)
+			elif re.match(r"\d+, ?\d+, ?\d+, ?\d+(?:;.*+)?", grab_mode):
+				name = ""
+				coords = grab_mode
+				if '\n' in grab_mode:
+					coords, name = grab_mode.split('\n', 1)
+					coords = tuple(map(int, coords.split(",")))
+					wx.CallAfter(
+						self.frame.capture_partial_screen, coords, name
+					)
+			else:
+				log.error(f"Invalid grab mode: {grab_mode}")
+		elif data.startswith("url:"):
+			name = ""
+			url = data.split(':', 1)[1].strip()
+			if '\n' in url:
+				url, name = url.split('\n', 1)
+				image_files = [
+					ImageFile(
+						location=url, name=name, size=-1, dimensions=(0, 0)
+					)
+				]
+				wx.CallAfter(self.frame.current_tab.add_images, image_files)
+		else:
+			log.error(f"no action for data: {data}")
 
 	def stop(self):
 		self.running.clear()
