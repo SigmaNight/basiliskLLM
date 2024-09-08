58dffaa41211e65c39958b74f53d8d62ebd89dbb
clementb49 <clement.boussiron@gmail.com>
2024-09-08 14:12:45 +0200

    feat(account): Add default account management in account config

--- basilisk/config/account_config.py
+++ basilisk/config/account_config.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 import logging
 from functools import cache, cached_property
 from os import getenv
-from typing import Any, Iterable, Optional
-from uuid import uuid4
+from typing import Any, Iterable, Optional, Union
+from uuid import UUID, uuid4
 
 import keyring
+from more_itertools import locate
 from pydantic import (
 	UUID4,
 	BaseModel,
@@ -218,6 +219,9 @@ class Account(BaseModel):
 		if self.api_key_storage_method == KeyStorageMethodEnum.system:
 			keyring.delete_password(APP_NAME, str(self.id))
 
+	def __eq__(self, value: Account) -> bool:
+		return self.id == value.id
+
 
 config_file_name = "accounts.yml"
 
@@ -231,6 +235,50 @@ class AccountManager(BasiliskBaseSettings):
 	model_config = get_settings_config_dict(config_file_name)
 
 	accounts: list[OnErrorOmit[Account]] = Field(default=list())
+
+	default_account_info: Optional[Union[UUID, str]] = Field(
+		default=None, union_mode="left_to_right"
+	)
+
+	@cached_property
+	def default_account(self) -> Account:
+		index = 0
+		if isinstance(self.default_account_info, UUID):
+			try:
+				return self[self.default_account_info]
+			except KeyError:
+				log.warning(
+					f"Default account not found for id {self.default_account_info} using the first account"
+				)
+		elif isinstance(
+			self.default_account_info, str
+		) and self.default_account_info.startswith("env:"):
+			provider_name = self.default_account_info[4:]
+			index = next(
+				locate(
+					self.accounts,
+					lambda x: x.provider.name == provider_name
+					and x.account_source == AccountSource.ENV_VAR,
+				),
+				None,
+			)
+			if index is None:
+				log.warning(
+					f"Default account not found in env variable for provider {provider_name} using the first account"
+				)
+				index = 0
+		return self.accounts[index]
+
+	def set_default_account(self, value: Optional[Account]):
+		if not value or not isinstance(value, Account):
+			self.default_account_info = None
+			del self.__dict__["default_account"]
+			return
+		if value.source == AccountSource.ENV_VAR:
+			self.default_account_info = f"env:{value.provider.name}"
+		else:
+			self.default_account_info = value.id
+		self.__dict__["default_account"] = value
 
 	@field_validator("accounts", mode="after")
 	@classmethod
@@ -318,11 +366,24 @@ class AccountManager(BasiliskBaseSettings):
 	def __iter__(self):
 		return iter(self.accounts)
 
-	def __getitem__(self, index) -> Account:
-		return self.accounts[index]
+	def __getitem__(self, index: Union[int, UUID]) -> Account:
+		if isinstance(index, int):
+			return self.accounts[index]
+		elif isinstance(index, UUID):
+			try:
+				return next(filter(lambda x: x.id == index, self.accounts))
+			except StopIteration:
+				raise KeyError(f"Account with id {index} not found")
 
-	def __setitem__(self, index, value):
-		self.accounts[index] = value
+	def __setitem__(self, index: Union[int, UUID], value: Account):
+		if isinstance(index, int):
+			self.accounts[index] = value
+		elif isinstance(index, UUID):
+			index = next(locate(self.accounts, lambda x: x.id == index), None)
+			if index is None:
+				self.accounts.append(value)
+			else:
+				self.accounts[index] = value
 
 	def save(self):
 		save_config_file(
