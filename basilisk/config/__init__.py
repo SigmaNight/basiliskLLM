696fd8b51aeb0b6de6fd36577fff1dce7571a1d5
clementb49 <clement.boussiron@gmail.com>
2024-09-14 23:59:16 +0200

    docs(account_config): remove comments which are considered useless by @aaclause

--- basilisk/config/__init__.py
+++ basilisk/config/__init__.py
@@ -12,26 +12,15 @@ from .main_config import BasiliskConfig
 from .main_config import get_basilisk_config as conf
 
 __all__ = [
-	# export function to get accounts configuration
 	"accounts",
-	# export Account class to allow new account creation
 	"Account",
-	# export AccountOrganization class to allow new account organization creation
 	"AccountOrganization",
-	# export GUI helper function to get display labels for account source
 	"get_account_source_labels",
-	# export function to get main configuration
 	"conf",
-	# export enum to allow configuration of key storage method
 	"KeyStorageMethodEnum",
-	# export enum which define the source of an account
 	"AccountSource",
-	# export enum which define the log level
 	"LogLevelEnum",
-	# export enum which define the release channel
 	"ReleaseChannelEnum",
-	# export enum which define the automatic update mode
 	"AutomaticUpdateModeEnum",
-	# export BasiliskConfig class to allow new configuration creation
 	"BasiliskConfig",
 ]
