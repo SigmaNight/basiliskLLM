7a4a9f0e7eb0ff6a8e5157f00de98a09d462a977
clementb49 <clement.boussiron@gmail.com>
2024-12-26 11:44:56 +0100

    refactor: update the measure_time decorator to update the logger and run the method imediately

--- basilisk/decorators.py
+++ basilisk/decorators.py
@@ -27,9 +27,15 @@ def ensure_no_task_running(method: Callable):
 def measure_time(method: Callable):
 	@wraps(method)
 	def wrapper(*args, **kwargs):
+		if not logger.isEnabledFor(logging.DEBUG):
+			return method(*args, **kwargs)
 		start = time.time()
+		module_name = method.__module__
+		qualname = method.__qualname__
 		result = method(*args, **kwargs)
-		logger.debug(f"{method.__name__} took {time.time() - start} seconds")
+		logger.debug(
+			f"{module_name}.{qualname} took {time.time() - start:.3f} seconds"
+		)
 		return result
 
 	return wrapper
