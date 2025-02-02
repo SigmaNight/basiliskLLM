1e891621370689e057200618966379296a971513
clementb49 <clement.boussiron@gmail.com>
2025-02-02 16:04:14 +0100

    docs: add docstring for the accessible output module

--- basilisk/accessible_output.py
+++ basilisk/accessible_output.py
@@ -1,21 +1,35 @@
+"""Functions for interacting with the Accessible Output library."""
+
 import logging
 import re
 from functools import cache
 
 import accessible_output3.outputs.auto
 
+from basilisk.decorators import measure_time
+
 log = logging.getLogger(__name__)
 
 
 @cache
-def get_accessible_output():
+@measure_time
+def get_accessible_output() -> accessible_output3.outputs.auto.Auto:
+	"""Initialize the Accessible Output library. Cache the result for future calls.
+
+	Returns:
+		An instance of the Accessible Output library
+	"""
 	log.info("Initializing Accessible Output")
 	return accessible_output3.outputs.auto.Auto()
 
 
 @cache
 def get_clean_steps() -> list[tuple[re.Pattern | str]]:
-	"""Return a list of regex patterns and their replacements to clean text."""
+	"""Return a list of regex patterns and their replacements to clean text.
+
+	Returns:
+		A list of tuples containing a compiled regex pattern and its replacement string.
+	"""
 	log.debug("Initializing clean steps")
 	return [
 		# Remove bold and italics
@@ -37,7 +51,14 @@ def get_clean_steps() -> list[tuple[re.Pattern | str]]:
 
 
 def clear_for_speak(text: str) -> str:
-	"""Remove common Markdown elements from text for accessible output."""
+	"""Remove common Markdown elements from text for accessible output.
+
+	Args:
+		text: The text to clean
+
+	Returns:
+		The cleaned text
+	"""
 	for pattern, replacement in get_clean_steps():
 		text = pattern.sub(replacement, text)
 	return text
