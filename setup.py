c358fde44ac8ce5325bf51f8b2ad5cbbcd3a9c79
clemetb49 <clement.boussiron@gmail.com>
2026-04-11 13:07:31 +0200

    fix: correct path inclusion for translation string extraction

--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ setup(
 	message_extractors={
 		"basilisk": [
 			("*.py", "python", None),
-			("gui/**.py", "python", None),
+			("config/**.py", "python", None),
+			("conversation/**.py", "python", None),
+			("ipc/**.py", "python", None),
+			("presenters/**.py", "python", None),
 			("provider_engine/**.py", "python", None),
+			("services/**.py", "python", None),
+			("singleton_instance/**.py", "python", None),
+			("views/**.py", "python", None),
 		]
 	}
 )
